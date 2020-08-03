d5a737903c1af10318096c3bc8d249866ff952bc
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-08-03 15:07:14 +0100

    binding event

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Welcome from './components/Welcome';
 import Counter from './components/Counter';
 import Message from './components/Message';
 import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick'
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind'
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick />
-      <ClassClick />
+      <EventBind />
+      {/* <FunctionClick />
+      <ClassClick /> */}
       {/* <Message /> */}
       {/* <Counter /> */}
       {/* <Greet name="iqbal" >
