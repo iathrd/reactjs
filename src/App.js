e3a3ec28ccc51dbd2a53773cd64d6e18d72f8859
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-08-05 13:31:04 +0100

    Pure Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import FragmentDemo from './components/FragmentDemo';
 import Tabel from './components/Tabel';
+import PureComp from './components/PureComp'
 
 
 
 function App() {
   return (
     <div className="App">
-      <Tabel />
+      <PureComp />
+      {/* <Tabel /> */}
       {/* <FragmentDemo /> */}
     </div>
   );
--- src/components/Colum.js
+++ src/components/Colum.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Colum() {
+    const item = []
     return (
         <React.Fragment>
             <td>Name</td>
